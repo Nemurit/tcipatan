ebe5a68e58ce3f1cb530cf3a7736900e0a590106
Nemurit <168225271+Nemurit@users.noreply.github.com>
2024-11-25 07:21:34 +0100

    Update ssp_tools.js

--- ssp_tools.js
+++ ssp_tools.js
@@ -797,7 +797,7 @@
     window.onload = function() {
         addExpandAllButton();
         getSTEM();
-        //containerCountButton();
+        containerCountButton();
     }
 
     function getSTEM() {
