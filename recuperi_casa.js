913954ca912a6d3ec97ac754ea26539f6684bd0d
Nemurit <168225271+Nemurit@users.noreply.github.com>
2024-12-02 01:06:10 +0100

    Update recuperi_casa.js

--- recuperi_casa.js
+++ recuperi_casa.js
@@ -133,7 +133,7 @@
 
         $('#contentContainer').remove();
 
-        const contentContainer = $('<div id="contentContainer" style="position: fixed; top: 10px; right: 10px; display: flex; flex-direction: column; gap: 20px;"></div>');
+        const contentContainer = $('<div id="contentContainer" style="position: fixed; top: 10px; right: 10px; height: 70vh; width: 400px; overflow-y: auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); background: white; padding: 10px; border: 1px solid #ddd;"></div>');
 
         if (Object.keys(sortedSummary).length === 0) {
             return;
@@ -233,7 +233,7 @@
                 table-layout: auto;
                 margin: 20px 0;
                 border-collapse: collapse;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                width: 100%;
             }
             #bufferSummaryTable th, #bufferSummaryTable td {
                 border: 1px solid #ddd;
@@ -273,4 +273,5 @@
         addToggleButton();
         fetchBufferSummary();
     });
+
 })();
